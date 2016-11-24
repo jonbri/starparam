ff44d1892de2eb15279b73a5612102caa23f4515
Jonathan Brink <jonathandavidbrink@gmail.com>
2016-11-24 11:55:33 -0500

    simplify folder structure

--- bin/package.sh
+++ bin/package.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-mkdir -p dist
 version=`grep version ./package.json | \
     awk -F "\"" '{ print $4 }'`
 
-echo "/* https://github.com/jonbri/starparam v$version" `date` "*/" > dist/starparam.min.js
-uglifyjs src/starparam.js --compress --mangle >> dist/starparam.min.js
-cp src/starparam.js dist/starparam.js
+echo "/* https://github.com/jonbri/starparam v$version" `date` "*/" > starparam.min.js
+uglifyjs starparam.js --compress --mangle >> starparam.min.js
 
